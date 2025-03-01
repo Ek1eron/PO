80099d674f9831a7ce79fbdbd1db1438a0084bbd
Ek1eron <143397838+Ek1eron@users.noreply.github.com>
2025-03-01 11:32:46 +0200

    lab 2 start

---
+++ lab2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <cstdlib>
+#include <iomanip>
+#include <thread>
+
+using namespace std;
+
+
+int main()
+{
+  cout<<"HI!"<<endl;
+
+    return 0;
+}
