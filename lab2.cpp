cf6f6a02cc408fe0c109e32d4f129cd6d68078dd
Ek1eron <143397838+Ek1eron@users.noreply.github.com>
2025-03-18 00:45:13 +0200

    fixes

--- lab2.cpp
+++ lab2.cpp
@@ -23,24 +23,36 @@ void generate_data(int size)
 
     arr.resize(size);
     for (int &num : arr)
-        {
+    {
         num = dist(gen);
     }
 }
 
-void work(int start, int end, int &local_count, int &local_min)
+void sequential_find(int &count, int &min_negative)
 {
-    local_count = 0;
-    local_min = INT_MAX;
+    count = 0;
+    min_negative = INT_MAX;
+    for (int i = 0; i < arr.size(); ++i)
+    {
+        if (arr[i] < 0)
+        {
+            count++;
+            if (arr[i] < min_negative)
+                min_negative = arr[i];
+        }
+    }
+}
+
+void work_mutex(int start, int end, int &count, int &min_negative, mutex &mtx)
+{
     for (int i = start; i < end; ++i)
     {
         if (arr[i] < 0)
         {
-            local_count++;
-            if (arr[i] < local_min)
-            {
-                local_min = arr[i];
-            }
+            lock_guard<mutex> lock(mtx);
+            count++;
+            if (arr[i] < min_negative)
+                min_negative = arr[i];
         }
     }
 }
@@ -53,30 +65,32 @@ void parallel_find_mutex(int &count, int &min_negative, int num_threads)
     vector<thread> threads;
     int base_chunk_size = arr.size() / num_threads;
     int remainder = arr.size() % num_threads;
-
-    vector<int> local_counts(num_threads, 0);
-    vector<int> local_mins(num_threads, INT_MAX);
-
     int start = 0;
+
     for (int i = 0; i < num_threads; ++i)
     {
         int chunk_size = base_chunk_size + (i < remainder ? 1 : 0);
         int end = start + chunk_size;
-        threads.emplace_back(work, start, end, ref(local_counts[i]), ref(local_mins[i]));
+        threads.emplace_back(work_mutex, start, end, std::ref(count), std::ref(min_negative), std::ref(mtx));
         start = end;
     }
     for (auto &t : threads)
     {
         t.join();
     }
+}
 
-    for (int i = 0; i < num_threads; ++i)
+void work_atomic(int start, int end, atomic<int> &count, atomic<int> &min_negative)
+{
+    for (int i = start; i < end; ++i)
     {
-        lock_guard<mutex> lock(mtx);
-        count += local_counts[i];
-        if (local_mins[i] < min_negative)
+        if (arr[i] < 0)
         {
-            min_negative = local_mins[i];
+            count.fetch_add(1, memory_order_relaxed);
+            int current = min_negative.load(memory_order_relaxed);
+            while (arr[i] < current && !min_negative.compare_exchange_weak(current, arr[i], memory_order_relaxed))
+            {
+            }
         }
     }
 }
@@ -88,34 +102,19 @@ void parallel_find_atomic(atomic<int> &count, atomic<int> &min_negative, int num
     vector<thread> threads;
     int base_chunk_size = arr.size() / num_threads;
     int remainder = arr.size() % num_threads;
-
     int start = 0;
+
     for (int i = 0; i < num_threads; ++i)
     {
         int chunk_size = base_chunk_size + (i < remainder ? 1 : 0);
         int end = start + chunk_size;
-
-        threads.emplace_back([&, start, end]()
-        {
-            int local_count = 0;
-            int local_min = INT_MAX;
-            work(start, end, local_count, local_min);
-            count.fetch_add(local_count, memory_order_relaxed);
-            int expected = min_negative.load();
-            while (local_min < expected && !min_negative.compare_exchange_weak(expected, local_min, memory_order_relaxed));
-        });
+        threads.emplace_back(work_atomic, start, end, std::ref(count), std::ref(min_negative));
         start = end;
     }
     for (auto &t : threads)
     {
         t.join();
     }
-}
-
-
-void sequential_find(int &count, int &min_negative)
-{
-    work(0, arr.size(), count, min_negative);
 }
 
 int main()
@@ -127,31 +126,31 @@ int main()
         int size = DATA_SIZES[i];
         generate_data(size);
 
-        int count, min_negative;
-        atomic<int> atomic_count, atomic_min_negative;
+        int seq_count, seq_min;
+        atomic<int> atomic_count, atomic_min;
 
         auto start = chrono::high_resolution_clock::now();
-        sequential_find(count, min_negative);
+        sequential_find(seq_count, seq_min);
         auto end = chrono::high_resolution_clock::now();
         cout << "Linear (Single Thread) - Data size: " << size << " - Execution time: "
              << fixed << setprecision(6) << chrono::duration<double>(end - start).count() << " sec\n"
-             << "Negative count: " << count << ", Minimum negative: " << min_negative << "\n\n";
+             << "Negative count: " << seq_count << ", Minimum negative: " << seq_min << "\n\n";
 
         for (int num_threads : THREAD_COUNTS)
         {
+            int mtx_count, mtx_min;
             start = chrono::high_resolution_clock::now();
-            parallel_find_mutex(count, min_negative, num_threads);
+            parallel_find_mutex(mtx_count, mtx_min, num_threads);
             end = chrono::high_resolution_clock::now();
             cout << "Mutex Parallel - Threads: " << num_threads << " - Data size: " << size << " - Execution time: "
                  << fixed << setprecision(6) << chrono::duration<double>(end - start).count() << " sec\n"
-                 << "Negative count: " << count << ", Minimum negative: " << min_negative << "\n\n";
-
+                 << "Negative count: " << mtx_count << ", Minimum negative: " << mtx_min << "\n\n";
             start = chrono::high_resolution_clock::now();
-            parallel_find_atomic(atomic_count, atomic_min_negative, num_threads);
+            parallel_find_atomic(atomic_count, atomic_min, num_threads);
             end = chrono::high_resolution_clock::now();
             cout << "Atomic CAS Parallel - Threads: " << num_threads << " - Data size: " << size << " - Execution time: "
                  << fixed << setprecision(6) << chrono::duration<double>(end - start).count() << " sec\n"
-                 << "Negative count: " << atomic_count << ", Minimum negative: " << atomic_min_negative << "\n\n";
+                 << "Negative count: " << atomic_count << ", Minimum negative: " << atomic_min << "\n\n";
         }
     }
 
